449e5ca4a252d4dd79b96f89868639d92a95a7e9
sou-komatsu <sou-komastu@mahoujin.co.jp>
2022-07-01 15:53:01 +0900

    prognostic time

--- py/functions.py
+++ py/functions.py
@@ -1,2 +1,25 @@
+from unittest import result
+
+
+import math
+
 def speed(x):
     return 2000 / x
+
+def speed_to_2000m_sec(speed):
+    return 2000 / speed
+
+def sec_to_2000m_time(sec):
+    m = math.floor(sec / 60)
+    s = round((sec % 60), 1)
+    if s < 10:
+        return str(m) + ':0' + str(s)
+    return str(m) + ':' + str(s)
+
+def sec_to_500m_lap_time(sec):
+    sec = sec / 4
+    m = math.floor(sec / 60)
+    s = round((sec % 60), 1)
+    if s < 10:
+        return str(m) + ':0' + str(s)
+    return str(m) + ':' + str(s)
--- py/prognostic_time_semifinal.py
+++ py/prognostic_time_semifinal.py
@@ -42,37 +42,23 @@ def calc_trends(df, boat_type):
     PT_1 = np.poly1d(res1)(2021)
     PT_2 = np.poly1d(res2)(2021)
     PT_3 = np.poly1d(res3)(2021)
-    #グラフ表示
-    plt.scatter(x, y, label='time')
-    plt.plot(x, y1, label='1d')
-    plt.plot(x, y2, label='2d')
-    plt.plot(x, y3, label='3d')
-    plt.xticks(np.arange(2000, 2022, 2))
-    plt.title(boat_type + " trends")
-    plt.xlabel('year', fontsize=12)  # x軸ラベル
-    plt.ylabel('speed[m/s]', fontsize=12)  # y軸ラベル
-    plt.grid()
-    plt.legend()
-    plt.figure()
-    plt.savefig('./../dst/prognostic_time/' + boat_type + '.jpg')
 
     return  median([PT_1, PT_2, PT_3])
 
 df = read_cev()
-df = df[df['section_code'].str.contains("決勝")]
-winner_df = df.groupby(['boat_type', 'year'], as_index=False)['2000m'].min()
-boat_types = (winner_df['boat_type'].unique())
+df = df[df['section_code'].str.contains("準決")]
+df = df.groupby(['boat_type', 'year'], as_index=False)['2000m'].quantile(0.75)
+boat_types = (df['boat_type'].unique())
 prognostic_time = []
 for boat_type in boat_types:
-    if (boat_type == 'w4x') | (boat_type == 'w4+'):
+    not_calc = ['m2+', 'w2-', 'w4x', 'w4+']
+    if boat_type in not_calc:
         prognostic_time.append(None)
         continue
-    prognostic_time.append(calc_trends(winner_df[winner_df['boat_type'] == boat_type], boat_type))
+    prognostic_time.append(calc_trends(df[df['boat_type'] == boat_type], boat_type))
 
 df_PT = pd.DataFrame({'speed[m/s]': prognostic_time}, index=boat_types)
-df_PT.loc['w4x', 'speed[m/s]'] = df_PT.loc['w4x+', 'speed[m/s]'] * (df_PT.loc['m4-', 'speed[m/s]']/df_PT.loc['m4+', 'speed[m/s]'])
-df_PT.loc['w4+', 'speed[m/s]'] = df_PT.loc['w4x', 'speed[m/s]'] * (df_PT.loc['m4+', 'speed[m/s]']/df_PT.loc['m4x', 'speed[m/s]'])
-
+df_PT = df_PT.dropna()
 time = []
 lap = []
 for speed in df_PT['speed[m/s]']:
@@ -82,4 +68,4 @@ for speed in df_PT['speed[m/s]']:
 
 df_PT['time'] = time
 df_PT['lap'] = lap
-df_PT.to_csv('./../dst/prognostic_time/PT_time.csv')
+df_PT.to_csv('./../dst/prognostic_time/PT_time_semifinal.csv')
